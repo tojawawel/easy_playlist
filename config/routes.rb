1357de24f9c4bbe2245f02ef8880ea4466251e4f
tojawawel <bartek.gawel@smartpension.co.uk>
2020-03-26 21:49:16 +0100

    add button for adding tracks

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: "homes#index"
+  get 'add_songs', to: 'playlists#add_songs_to_playlist'
 end
