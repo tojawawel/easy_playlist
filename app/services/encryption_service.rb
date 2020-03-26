2393cf42d1605383d4241e6dc0829b868a0b301e
tojawawel <bartek.gawel@smartpension.co.uk>
2020-03-26 21:44:17 +0100

    create encryption service

---
+++ app/services/encryption_service.rb
@@ -0,0 +1,22 @@
+class EncryptionService
+  SALT = Rails.application.credentials.salt
+  KEY = ActiveSupport::KeyGenerator.new('password').generate_key(SALT, 32).freeze
+
+  private_constant :KEY
+
+  delegate :encrypt_and_sign, :decrypt_and_verify, to: :encryptor
+
+  def self.encrypt(value)
+    new.encrypt_and_sign(value)
+  end
+
+  def self.decrypt(value)
+    new.decrypt_and_verify(value)
+  end
+
+  private
+
+  def encryptor
+    ActiveSupport::MessageEncryptor.new(KEY)
+  end
+end
