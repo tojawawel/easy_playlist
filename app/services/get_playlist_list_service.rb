c0ae6fb42d0c6af77db1a7274ed7f6fc11cfcb67
tojawawel <bartek.gawel@smartpension.co.uk>
2020-05-19 15:01:00 +0200

    get playlists list in view

--- app/services/get_playlist_list_service.rb
+++ app/services/get_playlist_list_service.rb
@@ -4,17 +4,30 @@ class GetPlaylistListService
 
   URL = 'https://api.spotify.com/v1/me/playlists?limit=50'
 
-  def playlists_names(auth_token, user)
-    list = []
-    playlists = download_playlists(auth_token)['items']
+  def initialize(auth_token, user)
+    @auth_token = auth_token
+    @user = user
+  end
 
+  def call
+    playlists_names
+  end
+
+  private
+
+  attr_reader :auth_token, :user
+
+  def playlists_names
+    list = []
+    playlists = JSON.parse(download_playlists.body, symbolize_names: true)[:items]
+    
     playlists.each do |playlist|
-      list << playlist['name'] if playlist['owner']['id'] == user.uid
+      list << playlist[:name] if playlist[:owner][:id] == user.uid
     end
     list
   end
 
-  def download_playlists(auth_token)
+  def download_playlists
     HTTParty.get(URL, headers: { Authorization: "Bearer #{auth_token}" })
   end
 
